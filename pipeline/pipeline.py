2be671f88d6f3a84c5afe6fade3470cdd0092409
biidy <nirinifitiavana@gmail.com>
2026-03-06 19:16:14 +0100

    Fix SyntaxError in pipeline.py: supprimer double noms

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,8 +4,8 @@ project = mlrun.new_project("projet1", context="./")
 mlrun.get_or_create_project("advertising-mlops")
 
 train_fn = mlrun.code_to_function(
-    "src/train.py",
-    name="train-model",
+    name="train-function",  # Soit tu le mets explicitement ici
+    filename="train.py",    # Assure-toi que le premier argument n'est pas déjà un string
     kind="job",
     image="mlrun/mlrun"
 )
